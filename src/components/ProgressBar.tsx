63e65445d598d54f17a373b9a7dd5b9d4fdcce4a
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-04-21 13:38:21 -0300

    criando popover para HabitDay

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,20 @@
+interface ProgressBarProps {
+	progress: number;
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+	const progressStyles = {
+		width: `${progress}%`
+	}
+	return (
+		<div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+			<div
+				role="progressbar"
+				aria-label="Progresso de hábitos competados nesse dia"
+				aria-valuenow={progress}
+				className="h-3 rounded-xl bg-sky-600"
+				style={progressStyles}
+			/>
+		</div>
+	);
+}
