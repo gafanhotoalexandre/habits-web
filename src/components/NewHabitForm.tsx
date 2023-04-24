784b4da5a9ae734a53d0bc055db74b70a8612482
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-04-24 09:41:37 -0300

    obtendo dados do formulario

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
 
@@ -10,8 +11,28 @@ const availableWeekDays = [
 ];
 
 export function NewHabitForm() {
+	const [title, setTitle] = useState('');
+	const [weekDays, setWeekDays] = useState<number[]>([]);
+
+	function createNewHabit(event: React.SyntheticEvent) {
+		event.preventDefault()
+		console.log(title, weekDays);
+	}
+
+	function handleToggleWeekDay(weekDay: number) {
+		if (weekDays.includes(weekDay)) {
+			const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+			setWeekDays(weekDaysWithRemovedOne);
+			return;
+		}
+		// add weekDay
+		const weekDaysWithAddedOne = [...weekDays, weekDay];
+		setWeekDays(weekDaysWithAddedOne);
+	}
+
 	return (
 		<form
+			onSubmit={createNewHabit}
 			className="w-full flex flex-col mt-6"
 		>
 			<label
@@ -27,6 +48,8 @@ export function NewHabitForm() {
 				placeholder="ex: Exercícios, dormir bem, etc..."
 				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
 				autoFocus
+				onChange={e => setTitle(e.target.value)}
+				value={title}
 			/>
 
 			<label
@@ -38,10 +61,11 @@ export function NewHabitForm() {
 
 			<div className="flex flex-col gap-2 mt-3">
 
-				{availableWeekDays.map(weekDay => (
+				{availableWeekDays.map((weekDay, index) => (
 					<Checkbox.Root
 						key={weekDay}
 						className="flex items-center gap-3 group"
+						onCheckedChange={() =>  handleToggleWeekDay(index)}
 					>
 						<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
 							<Checkbox.Indicator>
